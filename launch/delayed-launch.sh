ea9551f18ffb922b9e62b4d5b44af544441803c5
jeremyfromearth <jeremynealbrown@gmail.com>
2018-04-24 21:05:55 -0700

    Added third parameter to delayed launch script to indicate whether to show or hide the application

--- launch/delayed-launch.sh
+++ launch/delayed-launch.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
-echo "Waiting $1 second(s) before opening $2"
-sleep $1 
-open $2
+# $1 - delay before launching app
+# $2 - path to application file
+# $3 - show the application: true(default)|false 
+echo "Waiting $1 second(s) before launching $2"
+sleep $1
+
+if [ -z $3 ]; then
+  open $2
+else 
+  if [ $3 = "false" ]; then
+    open $2 -jg
+  else
+    open $2
+  fi
+fi
